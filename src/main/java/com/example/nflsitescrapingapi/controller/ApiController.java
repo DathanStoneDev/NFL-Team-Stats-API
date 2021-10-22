36c84e799d438f04b1d841611d8f85e68bb407d4
Dathan Stone <developer.stone@outlook.com>
2021-10-22 11:46:47 -0700

    Added custom descriptions in the Swagger UI configuration.

--- src/main/java/com/example/nflsitescrapingapi/controller/ApiController.java
+++ src/main/java/com/example/nflsitescrapingapi/controller/ApiController.java
@@ -5,6 +5,8 @@ import com.example.nflsitescrapingapi.stats.TeamPassingStats;
 import com.example.nflsitescrapingapi.stats.TeamReceivingStats;
 import com.example.nflsitescrapingapi.stats.TeamRushingStats;
 import com.example.nflsitescrapingapi.stats.TeamWinStats;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/nfl-stats/teams")
 @CrossOrigin
+@Api(tags = "NFL Stats Queries", value = "NFL Stats Queries")
 public class ApiController {
 
     @Autowired
@@ -19,24 +22,28 @@ public class ApiController {
 
     @GetMapping(value = "/win-stats/{year}")
     @ResponseBody
+    @ApiOperation(value = "Get a list of Win/Loss stats for all teams.")
     public List<TeamWinStats> getTeamWinStats(@PathVariable("year") int year) {
         return webScrapingService.getAllTeamWinStats(year);
     }
 
     @GetMapping(value = "/passing-stats/{side}/{year}")
     @ResponseBody
+    @ApiOperation(value = "Get a list of passing stats for all teams. Side parameter must be: 'offense' or 'defense'.")
     public List<TeamPassingStats> getTeamPassingStats(@PathVariable("side") String side, @PathVariable("year") int year) {
         return webScrapingService.getAllTeamPassingStats(side, year);
     }
 
     @GetMapping(value = "/rushing-stats/{side}/{year}")
     @ResponseBody
+    @ApiOperation(value = "Get a list of rushing stats for all teams. Side parameter must be: 'offense' or 'defense'.")
     public List<TeamRushingStats> getTeamRushingStats(@PathVariable("side") String side, @PathVariable("year") int year) {
         return webScrapingService.getAllTeamRushingStats(side, year);
     }
 
     @GetMapping(value = "/receiving-stats/{side}/{year}")
     @ResponseBody
+    @ApiOperation(value = "Get a list of receiving stats for all teams. Side parameter must be: 'offense' or 'defense'.")
     public List<TeamReceivingStats> getTeamReceivingStats(@PathVariable("side") String side, @PathVariable("year") int year) {
         return webScrapingService.getAllTeamReceivingStats(side, year);
     }
